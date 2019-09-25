6441a17079a94984ced0574c9c3c911ad9c73839
Álvaro <alvaroandresriquelme98@gmail.com>
2019-09-25 15:51:18 -0300

    actualizacion

--- listas.py
+++ listas.py
@@ -1,9 +1,19 @@
 from os import system
 
 nombres=[]
+apellidos=[]
 
 nombres.append("Álvaro")
 nombres.append("Wacoldo")
 nombres.append("Amada")
 
+apellidos.append("Gatica")
+apellidos.append("Soto")
+apellidos.append(input("Ingrese apellido: "))
+
 print(nombres)
+print(apellidos)
+
+for i in range(len(nombres)):
+    n=i+1
+    print(f"Persona {n}: {nombres[i]} {apellidos[i]}")
--- test.py
+++ test.py
@@ -5,21 +5,27 @@ from os import system
 
 def Pausa():
     print("Presione una tecla para continuar")
+    
 system("cls")
-def Sumar():
-    pass
 
-def Restar():
-    pass
+def Sumar(a,b):
+    print("La suma es :",str(a+b))
+    
+def Restar(x,y):
+    resta=x-y
+    print(f"La resta es {resta}")
+    
+def Multiplicar(q,w):
+    multi=q*w
+    print(f"La multiplicacion es {multi}")
 
-def Multiplicar():
-    pass
-
-def Dividir():
-    pass
+def Dividir(e,r):
+    divi=e/r
+    print(f"La division es {divi}")
 
 def Menu():
-    print("""1.Sumar
+    print("""SELECCIONE ALGUNA OPERACION
+    1.Sumar
     2.Restar
     3.Multiplicar
     4.Dividir
@@ -27,20 +33,25 @@ def Menu():
     
     
     while True:
-        num1=float(input("Ingrese un numero"))
-        num1=float(input("Ingrese otro numero"))
+        
         Menu()
+        
         op=int(input("Ingrese su opcion: "))
+        
+        if(op!=9 and op>=1 and op<=4):
+            num1=float(input("Ingrese un numero"))
+            num2=float(input("Ingrese otro numero"))
+            
         if op==1:
             Sumar(num1,num2)
         elif op==2:
-            Restar()
+            Restar(num1,num2)
         elif op==3:
-            Multiplicar()
+            Multiplicar(num1,num2)
         elif op==4:
-            Dividir()
+            Dividir(num1,num2)
         elif op==9:
-            print()"Fin del Programa)
+            print("Fin del Programa")
             Pausa()
             break
         else:
