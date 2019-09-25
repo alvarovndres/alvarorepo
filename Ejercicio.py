18c05a28b7355964973f2176525643ba230efec9
Álvaro <alvaroandresriquelme98@gmail.com>
2019-09-25 15:35:21 -0300

    mensaje

---
+++ Ejercicio.py
@@ -0,0 +1,16 @@
+from os import system
+# RUT.
+# NOMBRE.
+# COMUNA.
+# ACTIVO/INACTIVO.
+# BUSCAR POR RUT.
+#COMUNAS: CONCEPCION, TALCAHUANO, HUALPEN.
+
+while True:
+    print("""1. Ingresar Cliente"
+          2. Modificar Cliente
+          3. Eliminar Cliente
+          4. Buscar Cliente
+          5. Contar Clientes Inactivos
+          6. Contar Clientes por Comuna""")
+
